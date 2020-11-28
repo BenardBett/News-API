7ec11340c2339ac4034a844d11a03f473b484c66
BenardBett <kiplangatbett5@gmail.com>
2020-11-28 21:39:41 +0300

    moved the init file to app folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,28 @@
-from . import views, errors
+from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configuration
+app.config.from_object('DevConfig')
+app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+
+
+ return app
