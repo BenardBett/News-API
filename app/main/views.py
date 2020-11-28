4a84579f580fb03222e5d88b5ca055a743068ea6
BenardBett <kiplangatbett5@gmail.com>
2020-11-28 15:09:50 +0300

    created an app route and declared the the object instance

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,17 @@ from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    newsapi = NewsApiClient(api_key="d4df65110d1448f28e60b32fe558da44")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    url = []
+    img = []
 
 
 if __name__ == "__main__":
